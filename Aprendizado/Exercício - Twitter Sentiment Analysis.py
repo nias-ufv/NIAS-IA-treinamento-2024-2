87c95c8df75d7e6cf918ac826be82fb2393616e6
Victor R. Moreira <victor.moreira@ufv.br>
2025-02-09 22:02:02 -0300

    Exercicio Twitter Sentiment Iniciado

--- Aprendizado/Exercício - Twitter Sentiment Analysis.py
+++ Aprendizado/Exercício - Twitter Sentiment Analysis.py
@@ -1,9 +1,12 @@
 # Estudos em python
 
-
-a = int(input('Digite um número: '))
-
-if (a % 2 == 0):
-    print('Seu número é par')
-else:
-    print('Seu número é impar')
+tweets = [ "Wow, what a great day today!! #sunshine",
+"I feel sad about the things going on around us. #covid19",
+"I'm really excited to learn Python with @JovianML #zerotopandas",
+"This is a really nice song. #linkinpark",
+"The python programming language is useful for data science",
+"Why do bad things happen to me?",
+"Apple announces the release of the new iPhone 12. Fans are excited.",
+"Spent my day with family!! #happy",
+"Check out my blog post on common string operations in Python. #zerotopandas",
+"Freecodecamp has great coding tutorials. #skillup" ]
